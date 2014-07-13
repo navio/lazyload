f6c92b1d2f9c7c9b95190fd14ae3cbfcecb833f1
J. Alberto Navarro <alberto@hosts.mx>
2014-07-13 17:12:55 -0400

    Home De

---
+++ isVisible.js
@@ -0,0 +1,18 @@
+Object.prototype.isVisible = function(id){
+
+	var viewport = {
+		height: null,
+		width: null
+	};
+	
+	var bounds = {
+		x: (this.offsetLeft - this.scrollLeft + this.clientLeft), // Verify
+		y: (this.offsetTop - this.scrollTop + this.clientTop),	
+	};
+	
+	// Is it visible?
+	if ( )
+		
+	return 1;
+
+};
